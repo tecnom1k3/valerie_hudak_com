b60ea7f4db0bbdc4cb3b13a9797c156823d87018
tecnom1k3 <miguel@digitec.ws>
2017-10-11 20:51:46 -0700

    Features/form (#11)
    * adding contact info, improving aesthetics
    
    * integrated contact form with jwt and recaptcha
    
    * added api url to variable
    
    * adding modals for server response validation when submiting the form
    
    * fixing form submit, reset recaptcha on submit
    
    * added parametrized api endpoint, depending on the gulp task

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,79 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$.ajaxSetup({ cache: false });
+
+$(function () {
+
+    const apiUrl = '{{API_ENDPOINT}}';
+
+    /**
+     *
+     * @type {{getCSRF}}
+     */
+    const CSRFModule =  (function (jQuery) {
+
+        /**
+         *
+         * @param element
+         * @returns {string}
+         */
+        const getCSRF = function(element) {
+            const jsPromise = Promise.resolve(jQuery.ajax({
+                url: apiUrl + '/api/csrf',
+                jsonp: 'callback',
+                dataType: 'jsonp'
+            }));
+
+            jsPromise.then(function (response) {
+               element.val(response.csrf);
+            });
+        };
+
+        return {
+            getCSRF: getCSRF
+        }
+    })($);
+
+    const FormHandlerModule = (function (jQuery) {
+        const submitForm = function (form) {
+            jQuery('#modalWait').modal('show');
+
+            const jsPromise = Promise.resolve(jQuery.ajax({
+                url: apiUrl + '/api/form',
+                jsonp: 'callback',
+                dataType: 'json',
+                type: 'POST',
+                crossDomain: true,
+                data: form.serialize()
+            }));
+
+            jsPromise.then(function (data) {
+                jQuery('#modalWait').modal('hide');
+                if (data.success) {
+                    jQuery('#modalSuccess').modal('show');
+                } else {
+                    jQuery('#modalError').modal('show');
+                }
+            }, function (reason) {
+                jQuery('#modalWait').modal('hide');
+                jQuery('#modalError').modal('show');
+            });
+        };
+
+        return {
+            submitForm: submitForm
+        }
+    })($);
+
+    $('#name').focus(function() {
+        if ($('#token').val() === '') {
+            CSRFModule.getCSRF($('#token'));
+        }
+    });
+
+    $('#commentForm').submit(function (e) {
+        e.preventDefault();
+        FormHandlerModule.submitForm($(this));
+        grecaptcha.reset();
+    });
+});
